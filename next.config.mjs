469e3aac34297d59581048927af58c37482562b2
Iliavsevolodov <78146096+Iliavsevolodov@users.noreply.github.com>
2026-07-08 03:16:24 +0300

    Configure static export for GitHub Pages

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
+
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: isGithubPages ? '/kaskada' : '',
+  assetPrefix: isGithubPages ? '/kaskada' : ''
+};
+
+export default nextConfig;
